0bfe61f52f8aac042333225bc433427e78295212
JPCatarino <jorge.catarino@ua.pt>
2020-05-29 03:44:41 +0100

    [COMMON] Add Message type attribute to Message

--- src/common/Message.java
+++ src/common/Message.java
@@ -2,5 +2,11 @@ package common;
 
 public class Message {
 
+    private MessageType messageType;
+
     public Message(){}
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
 }
