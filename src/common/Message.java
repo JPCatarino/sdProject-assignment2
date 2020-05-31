7579915f4d5027d2aff2451673d1b110541b3d49
JPCatarino <jorge.catarino@ua.pt>
2020-05-31 04:36:35 +0100

    [COMMON] Add boolean attribute to message

--- src/common/Message.java
+++ src/common/Message.java
@@ -4,7 +4,17 @@ public class Message {
 
     private MessageType messageType;
 
+    private Boolean booleanValue1;
+
     public Message(){}
+
+    public Boolean getBooleanValue1() {
+        return booleanValue1;
+    }
+
+    public void setBooleanValue1(Boolean booleanValue1) {
+        this.booleanValue1 = booleanValue1;
+    }
 
     public void setMessageType(MessageType messageType) {
         this.messageType = messageType;
