0bb658f1fff5694f84ca3bbb2f0db8f83c2aa645
JPCatarino <jorge.catarino@ua.pt>
2020-06-03 04:08:03 +0100

    [STUBS] BCP

--- src/stubs/BagColPointStub.java
+++ src/stubs/BagColPointStub.java
@@ -23,14 +23,40 @@ public class BagColPointStub extends SharedRegionStub {
     }
 
     public void carryItToAppropriateStore(int [] bag){
+        Message newMessage = new Message();
 
+        newMessage.setMessageType(MessageType.CARRYITTOAPPROPRIATESTORE);
+        newMessage.setBag1(bag);
+
+        ClientCom cc = new ClientCom(super.getServerHostName(),super.getServerPort());
+        cc.open();
+        cc.writeObject(newMessage);
+
+        newMessage =(Message) cc.readObject();
     }
 
     public void setNoMoreBags(boolean noMoreBags){
+        Message newMessage = new Message();
 
+        newMessage.setMessageType(MessageType.SETNOMOREBAGS);
+        newMessage.setBooleanValue1(noMoreBags);
+
+        ClientCom cc = new ClientCom(super.getServerHostName(),super.getServerPort());
+        cc.open();
+        cc.writeObject(newMessage);
+
+        newMessage =(Message) cc.readObject();
     }
 
     public void resetBagColPoint(){
+        Message newMessage = new Message();
 
+        newMessage.setMessageType(MessageType.RESETBAGCOLPOINT);
+
+        ClientCom cc = new ClientCom(super.getServerHostName(),super.getServerPort());
+        cc.open();
+        cc.writeObject(newMessage);
+
+        newMessage =(Message) cc.readObject();
     }
 }
