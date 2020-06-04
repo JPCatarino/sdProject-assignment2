b951a348ad0fc4cf07443f9a45f2e9a021927418
JPCatarino <jorge.catarino@ua.pt>
2020-06-04 03:33:55 +0100

    [PROXY] DepQuay Proxy

--- src/proxies/DepartureQuayProxy.java
+++ src/proxies/DepartureQuayProxy.java
@@ -20,7 +20,22 @@ public class DepartureQuayProxy implements SharedRegionProxy {
 
     @Override
     public Message processAndReply(Message msg) {
-        return null;
+        Message nm = new Message();
+        ServiceProviderProxy serviceProviderProxy = (ServiceProviderProxy) Thread.currentThread();
+
+        switch (msg.getMessageType()){
+            case PARKTHEBUSANDLETPASSOFF:
+                departureQuay.parkTheBusAndLetPassOff();
+                break;
+            case GOTOARRIVALTERMINAL:
+                departureQuay.goToArrivalTerminal();
+                break;
+            case LEAVETHEBUS:
+                departureQuay.leaveTheBus();
+                break;
+        }
+
+        return nm;
     }
 
     @Override
