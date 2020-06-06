82ac8b46d5138404e97215b72bf11037a8863296
fabioal0 <fabioal0@hotmail.com>
2020-06-06 19:17:36 +0100

    Revert "Merge branch 'master' of https://github.com/JPCatarino/sdProject-assignment2"
    This reverts commit 8eb4b74fe45e5b5f66bcaf5c7948a77b28efce43, reversing
    changes made to 0a8d976e8d4677c67df9c2cac39df3c3dc9e8b2e.

--- src/main/ClientAirportRhapsody.java
+++ src/main/ClientAirportRhapsody.java
@@ -129,7 +129,6 @@ public class ClientAirportRhapsody {
             }
             // check if porter has collected all baggage
             while(arrivalLounge.ispWake() && i+1 != K_landings);
-            plainBags = new ArrayList<>();
         }
 
         try {
